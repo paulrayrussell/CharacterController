618c81ac63a52b3285c6ad09a416abb7fbbc7e12
Paul Russell <paulrayrussell@gmail.com>
2020-04-23 13:45:29 +0100

    obj pool

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab = null;
+    [SerializeField] private int initialNoRequired = 5;
+    private List<BulletStruct> pooledObjects = null;
+
+    public struct BulletStruct
+    {
+        public readonly GameObject bulletPrefab;
+        public readonly PowerEnemyBullet bulletScript;
+
+        public BulletStruct(GameObject o, PowerEnemyBullet bs)
+        {
+            bulletPrefab = o;
+            bulletScript = bs;
+        }
+    }
+    void Start()
+    {  
+        pooledObjects = new List<BulletStruct>();
+        for (int i = 0; i < initialNoRequired; i++)
+        {
+            CreateNew();
+        }
+        
+    }
+    
+    public BulletStruct GetPooledObject() {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].bulletPrefab.activeInHierarchy) {
+                return pooledObjects[i];
+            }
+        }
+
+        return CreateNew();
+    }
+
+    private BulletStruct CreateNew()
+    {
+         GameObject obj = Instantiate(bulletPrefab);
+         obj.transform.parent = gameObject.transform;
+         obj.SetActive(false);
+         BulletStruct bs = new BulletStruct(obj, obj.GetComponent<PowerEnemyBullet>());
+         pooledObjects.Add(bs);
+         return bs;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerAnimationController bodyAnimationCont = null;
     [SerializeField] private ArmsRotationController armsRotationCont = null;
     [SerializeField] private GameObject armsAnimatorGO = null;
+    [SerializeField] private ObjectPooler bulletPooler = null;
     [SerializeField] private Transform leftFirept = null;
     [SerializeField] private float armOffset = 0.1f;
-    [SerializeField] private GameObject bullet = null;
 
     private CharController charController;
     private float xMovVel = 0.5f;
     private float yMovVel = 35f;
     private SpriteRenderer srOuter, srInner;
+    private bool firing;
 
 
     void Start()
@@ -27,17 +28,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) & charController.state == CharController.CharacterState.GROUNDED)
+        if (Input.GetKey(KeyCode.RightArrow) & charController.state == CharController.CharacterState.GROUNDED && !charController.isShooting)
         {
             charController.vel.x += xMovVel * Time.deltaTime; //must have dt - as will vary
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) & charController.state == CharController.CharacterState.GROUNDED)
+        if (Input.GetKey(KeyCode.LeftArrow) & charController.state == CharController.CharacterState.GROUNDED && !charController.isShooting)
         {
             charController.vel.x -= xMovVel * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.Space) & charController.state == CharController.CharacterState.GROUNDED)
+        if (Input.GetKey(KeyCode.Space) & charController.state == CharController.CharacterState.GROUNDED && !charController.isShooting)
         {
             if (charController.state == CharController.CharacterState.GROUNDED)
             {
@@ -48,28 +49,13 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (charController.isShooting) return;
             if (charController.state != CharController.CharacterState.GROUNDED) return;
 
             Vector3 mouseClickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 shootDir = mouseClickPos - leftFirept.position;
 
-            if (shootDir.x > 0 && !bodyAnimationCont.spriteFacingRight) return;
-            if (shootDir.x < 0 && bodyAnimationCont.spriteFacingRight) return;
-            
-            srInner.enabled = true; //show the arms     
-            srOuter.enabled = true;     
-            
-            armsAnimatorGO.transform.localScale = bodyAnimationCont.spriteFacingRight ? //flip the scale to mirror arms
-                new Vector3( armsAnimatorGO.transform.localScale.x, -1, armsAnimatorGO.transform.localScale.z) : 
-                new Vector3( armsAnimatorGO.transform.localScale.x, 1, armsAnimatorGO.transform.localScale.z);
-
-            armsAnimatorGO.transform.position = bodyAnimationCont.spriteFacingRight ? //offset the arms for left or right
-                new Vector3(armsAnimatorGO.transform.position.x + armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z) :
-                new Vector3(armsAnimatorGO.transform.position.x - armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z);
-            
-            armsRotationCont.clickpt = mouseClickPos;
-            
-            StartCoroutine(FirePause(shootDir));
+            StartCoroutine(FirePause(shootDir, mouseClickPos));
             
             int enLayer = LayerMask.NameToLayer("Enemies");
             int enBitMask = 1 << enLayer;
@@ -83,22 +69,45 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private IEnumerator FirePause(Vector3 shootDir)
+    private IEnumerator FirePause(Vector3 shootDir, Vector3 mouseClickPos)
     {
         charController.isShooting = true;
+
+        if (shootDir.x > 0 && !bodyAnimationCont.spriteFacingRight) yield break;
+        if (shootDir.x < 0 && bodyAnimationCont.spriteFacingRight) yield break;
+
+
+        armsAnimatorGO.transform.localScale = bodyAnimationCont.spriteFacingRight ? //flip the scale to mirror arms
+            new Vector3( armsAnimatorGO.transform.localScale.x, -1, armsAnimatorGO.transform.localScale.z) : 
+            new Vector3( armsAnimatorGO.transform.localScale.x, 1, armsAnimatorGO.transform.localScale.z);
+
+        armsAnimatorGO.transform.position = bodyAnimationCont.spriteFacingRight ? //offset the arms for left or right
+            new Vector3(armsAnimatorGO.transform.position.x + armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z) :
+            new Vector3(armsAnimatorGO.transform.position.x - armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z);
+
+        armsRotationCont.clickpt = mouseClickPos;
+
+        srInner.enabled = true; //show the arms     
+        srOuter.enabled = true;
         yield return new WaitForSeconds(0.1f);
-        GameObject b = Instantiate(bullet, leftFirept.position, armsRotationCont.transform.rotation);
-        b.GetComponent<PowerEnemyBullet>().Setup(shootDir.normalized, 0.2f);
-        yield return new WaitForSeconds(0.5f);
         
-        charController.isShooting = false;
+        ObjectPooler.BulletStruct bs = bulletPooler.GetPooledObject();
+        bs.bulletPrefab.transform.rotation = armsRotationCont.transform.rotation;
+        bs.bulletPrefab.transform.position = leftFirept.position;
+        bs.bulletPrefab.SetActive(true);
+        bs.bulletScript.Setup(shootDir.normalized, 0.2f);
+        
+        yield return new WaitForSeconds(0.5f);
+
         //hide arms
         srInner.enabled = false;
         srOuter.enabled = false;
-        
+
         //put arms back to original offset
         armsAnimatorGO.transform.position = bodyAnimationCont.spriteFacingRight
             ? new Vector3(armsAnimatorGO.transform.position.x - armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z)
             : new Vector3(armsAnimatorGO.transform.position.x + armOffset, armsAnimatorGO.transform.position.y, armsAnimatorGO.transform.position.z);
+        
+        charController.isShooting = false;
     }
 }
